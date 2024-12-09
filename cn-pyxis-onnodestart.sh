ff11358cc14b25e71ce1aff0d8b4023e734c8d4c
Yeo Su Yia, Denise <dyeosy98@gmail.com>
2024-12-09 15:04:34 +0800

    Update cn-pyxis-onnodestart.sh

--- cn-pyxis-onnodestart.sh
+++ cn-pyxis-onnodestart.sh
@@ -24,22 +24,3 @@ sudo mkdir -p $PYXIS_RUNTIME_DIR
 sudo chmod 1777 $PYXIS_RUNTIME_DIR
 
 set -exo pipefail
-
-### Install NVIDIA libnvidia-container
-
-apt update
-
-nvidia-smi && export GPU_PRESENT=0 || GPU_PRESENT=-1;
-if [ $GPU_PRESENT -eq 0 ]; then
-    nvidia-container-cli info && export GPU_CONTAINER_PRESENT=0 || export GPU_CONTAINER_PRESENT=-1
-else
-    export GPU_CONTAINER_PRESENT=1
-fi
-
-if [ $GPU_PRESENT -eq 0 ] && [ $GPU_CONTAINER_PRESENT -gt 0 ]; then
-  distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-  curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
-  curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-  apt-get update -y
-  apt-get install libnvidia-container-tools -y
-fi
