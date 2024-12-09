5c33326452b40f923f471b028f24049142475065
Yeo Su Yia, Denise <dyeosy98@gmail.com>
2024-12-09 12:40:12 +0800

    Update postinstall.sh for head node to enable Pyxis and Enroot

--- postinstall.sh
+++ postinstall.sh
@@ -10,3 +10,32 @@ if [ ! -d "/opt/nvidia/nvidia_hpc_benchmarks_openmpi" ]; then
   sudo apt-get update
   sudo apt-get -y install nvidia-hpc-benchmarks-openmpi
 fi
+
+# HEAD NODE: Enable Pyxis and Enroot on AWS ParallelCluster 3.11.1
+
+set -e
+
+echo "Executing $0"
+
+# Configure Enroot
+ENROOT_PERSISTENT_DIR="/var/enroot"
+ENROOT_VOLATILE_DIR="/run/enroot"
+
+sudo mkdir -p $ENROOT_PERSISTENT_DIR
+sudo chmod 1777 $ENROOT_PERSISTENT_DIR
+sudo mkdir -p $ENROOT_VOLATILE_DIR
+sudo chmod 1777 $ENROOT_VOLATILE_DIR
+sudo mv /opt/parallelcluster/examples/enroot/enroot.conf /etc/enroot/enroot.conf
+sudo chmod 0644 /etc/enroot/enroot.conf
+
+# Configure Pyxis
+PYXIS_RUNTIME_DIR="/run/pyxis"
+
+sudo mkdir -p $PYXIS_RUNTIME_DIR
+sudo chmod 1777 $PYXIS_RUNTIME_DIR
+
+sudo mkdir -p /opt/slurm/etc/plugstack.conf.d/
+sudo mv /opt/parallelcluster/examples/spank/plugstack.conf /opt/slurm/etc/
+sudo mv /opt/parallelcluster/examples/pyxis/pyxis.conf /opt/slurm/etc/plugstack.conf.d/
+sudo -i scontrol reconfigure                        
+                    
